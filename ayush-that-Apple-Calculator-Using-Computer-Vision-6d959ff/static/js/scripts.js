5bec6cf6e7db7699c9d4e048042b0b5763cc4803
Shubham Singh <shubhsoch@gmail.com>
2025-01-26 16:42:00 +0530

    update

--- ayush-that-Apple-Calculator-Using-Computer-Vision-6d959ff/static/js/scripts.js
+++
@@ -1,7 +0,0 @@
-function fetchGeminiOutput() {
-  fetch("/gemini")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("gemini-output").innerText = data;
-    });
-}
